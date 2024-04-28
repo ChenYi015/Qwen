5167de8c9897d72e99ef8238fc97bc16ff91dfe1
Yi Chen <github@chenyicn.net>
2024-04-28 14:54:07 +0800

    Add web demo for model with peft adapater
    Signed-off-by: Yi Chen <github@chenyicn.net>

--- finetune/finetune_lora_single_gpu.sh
+++ finetune/finetune_lora_single_gpu.sh
@@ -5,6 +5,7 @@ MODEL="Qwen/Qwen-7B" # Set the path if you do not want to load from huggingface 
 # ATTENTION: specify the path to your training data, which should be a json file consisting of a list of conversations.
 # See the section for finetuning in README for more information.
 DATA="path_to_data"
+OUTPUT_DIR="output_qwen"
 
 function usage() {
     echo '
@@ -14,52 +15,60 @@ Usage: bash finetune/finetune_lora_single_gpu.sh [-m MODEL_PATH] [-d DATA_PATH]
 
 while [[ "$1" != "" ]]; do
     case $1 in
-        -m | --model )
-            shift
-            MODEL=$1
-            ;;
-        -d | --data )
-            shift
-            DATA=$1
-            ;;
-        -h | --help )
-            usage
-            exit 0
-            ;;
-        * )
-            echo "Unknown argument ${1}"
-            exit 1
-            ;;
+    -m | --model)
+        shift
+        MODEL=$1
+        ;;
+    -d | --data)
+        shift
+        DATA=$1
+        ;;
+    -o | --output)
+        shift
+        OUTPUT_DIR=$1
+        ;;
+    -h | --help)
+        usage
+        exit 0
+        ;;
+    *)
+        echo "Unknown argument ${1}"
+        exit 1
+        ;;
     esac
     shift
 done
 
+set -eux
+
 export CUDA_VISIBLE_DEVICES=0
 
+mkdir -p $OUTPUT_DIR
+
 python finetune.py \
-  --model_name_or_path $MODEL \
-  --data_path $DATA \
-  --bf16 True \
-  --output_dir output_qwen \
-  --num_train_epochs 5 \
-  --per_device_train_batch_size 2 \
-  --per_device_eval_batch_size 1 \
-  --gradient_accumulation_steps 8 \
-  --evaluation_strategy "no" \
-  --save_strategy "steps" \
-  --save_steps 1000 \
-  --save_total_limit 10 \
-  --learning_rate 3e-4 \
-  --weight_decay 0.1 \
-  --adam_beta2 0.95 \
-  --warmup_ratio 0.01 \
-  --lr_scheduler_type "cosine" \
-  --logging_steps 1 \
-  --report_to "none" \
-  --model_max_length 512 \
-  --lazy_preprocess True \
-  --gradient_checkpointing \
-  --use_lora
+    --model_name_or_path $MODEL \
+    --data_path $DATA \
+    --bf16 True \
+    --output_dir ${OUTPUT_DIR} \
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 10 \
+    --learning_rate 3e-4 \
+    --weight_decay 0.1 \
+    --adam_beta2 0.95 \
+    --warmup_ratio 0.01 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --report_to "none" \
+    --model_max_length 512 \
+    --lazy_preprocess True \
+    --gradient_checkpointing \
+    --use_lora
 
 # If you use fp16 instead of bf16, you should use deepspeed
 # --fp16 True --deepspeed finetune/ds_config_zero2.json
